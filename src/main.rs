72b93f75643b7019baed06084847fe482454a791
sizkern <sizekernel@gmail.com>
2024-11-14 19:08:45 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use reqwest::blocking::Client;
+
+fn main() {
+    let client = Client::new();
+
+    for i in 0..20 {
+        let url = format!("http://testphp.vulnweb.com/artists.php?artist={}", i);
+        match client.get(&url).send() {
+            Ok(response) => {
+                if let Ok(text) = response.text() {
+                    if text.contains("lyzae") {
+                        println!("{}", url);
+                    }
+                }
+            },
+            Err(e) => println!("{}: {}", i, e),
+        }
+    }
+}
